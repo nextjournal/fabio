015c8533c2b3dea6e0c64f30df407d3d317a8fe3
Frank Schroeder <frank.schroeder@gmail.com>
2017-07-25 11:11:09 +0200

    Issue #320: Retry metrics during startup
    This patch retries configuring metrics during startup
    to mitigate a race between fabio and metrics availability.
    
    Fixes #320

--- main.go
+++ main.go
@@ -276,12 +276,24 @@ func initMetrics(cfg *config.Config) {
 		return
 	}
 
+	var deadline = time.Now().Add(cfg.Registry.Timeout)
 	var err error
-	if metrics.DefaultRegistry, err = metrics.NewRegistry(cfg.Metrics); err != nil {
-		exit.Fatal("[FATAL] ", err)
-	}
-	if route.ServiceRegistry, err = metrics.NewRegistry(cfg.Metrics); err != nil {
-		exit.Fatal("[FATAL] ", err)
+	for {
+		metrics.DefaultRegistry, err = metrics.NewRegistry(cfg.Metrics)
+		if err == nil {
+			route.ServiceRegistry, err = metrics.NewRegistry(cfg.Metrics)
+		}
+		if err == nil {
+			return
+		}
+		if time.Now().After(deadline) {
+			exit.Fatal("[FATAL] ", err)
+		}
+		log.Print("[WARN] Error initializing metrics. ", err)
+		time.Sleep(cfg.Registry.Retry)
+		if atomic.LoadInt32(&shuttingDown) > 0 {
+			exit.Exit(1)
+		}
 	}
 }
 
